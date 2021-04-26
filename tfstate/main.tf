8da0157ff929670cb55c5d3a43d24299592874de
Arkoprabho Chakraborti <>
2021-04-26 22:54:27 +0530

    MODIFY: Enable server side encryption of the files

--- tfstate/main.tf
+++ tfstate/main.tf
@@ -22,4 +22,13 @@ resource "aws_s3_bucket" "state_bucket" {
   versioning {
     enabled = true
   }
+
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
