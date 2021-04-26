d0933115896ede3068c17de435615ecd4f7d725d
Arkoprabho Chakraborti <>
2021-04-26 22:45:33 +0530

    MODIFY: Add random resource
    We will be using this resource to append to the S3 bucket that we will be creating later

--- tfstate/main.tf
+++ tfstate/main.tf
@@ -2,3 +2,9 @@ provider "aws" {
   region = "us-east-1"
   profile = "personal"
 }
+
+resource "random_string" "random_sixteen" {
+  length           = 16
+  special          = false
+  upper            = false
+}
