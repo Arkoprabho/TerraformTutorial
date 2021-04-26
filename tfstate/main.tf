ba317d7e402f014589e230fad8c7384016211ba2
Arkoprabho Chakraborti <>
2021-04-26 23:02:00 +0530

    MODIFY: Change billing_mode to PAY_PER_REQUEST

--- tfstate/main.tf
+++ tfstate/main.tf
@@ -35,6 +35,7 @@ resource "aws_s3_bucket" "state_bucket" {
 
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "terraform-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
   attribute {
