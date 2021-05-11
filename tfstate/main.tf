80953fb89a7efc0715c6b33f50ecd3bd687619e2
Arkoprabho Chakraborti <>
2021-05-11 18:25:44 +0530

    MODIFY: Use S3 related variables instead of hardcoding them

--- tfstate/main.tf
+++ tfstate/main.tf
@@ -14,20 +14,20 @@ resource "aws_s3_bucket" "state_bucket" {
 
   # Prevent accidental deletion of this S3 bucket
   lifecycle {
-    prevent_destroy = true
+    prevent_destroy = var.prevent_destroy
   }
 
   # Enable versioning so we can see the full revision history of our
   # state files
   versioning {
-    enabled = true
+    enabled = var.enable_versioning
   }
 
   # Enable server-side encryption by default
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
+        sse_algorithm = var.encryption_algorithm
       }
     }
   }
